ceacb13866fb1013623152ad6ca878bc54a6dc2d
VladimirGlushchenko <105595545+VladimirGlushchenko@users.noreply.github.com>
2022-12-11 22:02:05 +0300

    step four

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+/* eslint-disable import/named */
+/* eslint-disable import/extensions */
 
-import { questionUserName } from "../src/cli.js";
+import { questionUserName } from '../src/cli.js';
 
 questionUserName();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,12 @@
-import readlineSync from "readline-sync";
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-console */
+
+import readlineSync from 'readline-sync';
 
 export const questionUserName = () => {
-  console.log("Welcome to the Brain Games!");
+  console.log('Welcome to the Brain Games!');
 
-  const name = readlineSync.question("May I have your name? ");
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
+
